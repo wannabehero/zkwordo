76a2d381c353f12c4f768f56908b80511f1c3f36
Sergey Pronin <sergey@pronin.me>
2023-06-09 18:08:30 +0100

    api addr

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['https://zkwordo.xyz', 'http://localhost:5173'],
+    },
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? '8000');
 }
